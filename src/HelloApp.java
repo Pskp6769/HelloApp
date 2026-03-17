e4d241bb2938fe9a9a40c086959447bc8c77d583
pranavpoola69-stack <pranavpoola69@gmail.com>
2026-03-17 15:46:50 +0530

    Merged UC5 with Dev

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,6 +1,19 @@
 public class HelloApp {
-    public static void main (String [] args)
-    {
-        System.out.println("Hello App Welcomed ");
+    public static void main(String[] args) {
+        String name = "World";
+
+        if (args.length > 0) {
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+            for (String arg : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(arg);
+                first = false;
+            }
+            name = nameBuilder.toString();
+        }
+        System.out.println("Hello, " + name + "!");
     }
 }
