c518bda6b5a4bf62a5e317eacf16a33e33159f92
pranavpoola69-stack <pranavpoola69@gmail.com>
2026-03-17 09:33:53 +0530

    Displayed Hello with Multiple Command-Line Arguments using Enhanced For Loop or Default Message

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,17 +1,18 @@
 public class HelloApp {
     public static void main(String[] args) {
         String name = "World";
+
         if (args.length > 0) {
-           StringBuilder nameBuilder = new StringBuilder();
-           for (int i = 0; i < args.length; i++){
-               nameBuilder.append(args[i]);
-
-               if (i < args.length -1){
-                   nameBuilder.append(",");
-               }
-
-           }
-           name = nameBuilder.toString();
+            StringBuilder nameBuilder = new StringBuilder();
+            boolean first = true;
+            for (String arg : args) {
+                if (!first) {
+                    nameBuilder.append(", ");
+                }
+                nameBuilder.append(arg);
+                first = false;
+            }
+            name = nameBuilder.toString();
         }
         System.out.println("Hello, " + name + "!");
     }
